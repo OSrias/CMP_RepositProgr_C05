bdd5208365385f24171a966e4134b120ff1cada7
OSrias <97224888+OSrias@users.noreply.github.com>
2022-01-07 12:14:27 +0000

    Add files via upload

---
+++ D13_UnitTest_Client/BankAccount.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace BankAccountClient
+{
+
+    public class BankAccount
+    {
+
+        #region Constants
+        public const string DebitAmountExceedsBalanceMessage = "Debit amount exceeds balance";
+        public const string DebitAmountLessThanZeroMessage = "Debit amount is less than zero";
+        #endregion
+
+        #region Constructor
+        public BankAccount(string customerName, double balance)
+        {
+
+            CustomerName = customerName;
+            Balance = balance;
+
+        }
+        #endregion
+
+        #region Properties
+        public string CustomerName { get; }
+        public double Balance { get; set; }
+        #endregion
+
+        #region Methods
+        public void Debit(double amount)
+        {
+
+            if (amount > Balance)
+            {
+                throw new System.ArgumentOutOfRangeException("amount", amount, DebitAmountExceedsBalanceMessage);
+            }
+            else if (amount < 0)
+            {
+                throw new System.ArgumentOutOfRangeException("amount", amount, DebitAmountLessThanZeroMessage);
+            }
+
+            // Erro
+            Balance += amount; // intentionally incorrect code
+
+            // Correction
+            // Balance -= amount;
+
+        }
+
+        public void Credit(double amount)
+        {
+
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            Balance += amount;
+
+        }        
+        #endregion
+
+    }
+
+}
---
+++ D13_UnitTest_Client/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BankAccountClient
+{
+
+    // TODO MRS: https://docs.microsoft.com/en-us/visualstudio/test/walkthrough-creating-and-running-unit-tests-for-managed-code?view=vs-2022
+
+    class Program
+    {
+    
+        static void Main(string[] args)
+        {
+
+            BankAccount bankAccount = new BankAccount("Mr. Bryan Walton", 11.99);
+
+            bankAccount.Credit(5.77);
+            bankAccount.Debit(11.22);
+
+            Console.WriteLine("Current balance is ${0}", bankAccount.Balance);
+
+        }
+
+    }
+
+}
